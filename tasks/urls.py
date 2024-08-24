922532f1c4c925ce84516032e33e8529f258a8b4
earnify123 <earnify123@gmail.com>
2024-08-24 08:28:29 +0600

    Update urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,1 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.task_list, name='task_list'),
+    path('complete_task/<int:task_id>/', views.complete_task, name='complete_task'),
+    path('profile/', views.profile, name='profile'),
+]
 
