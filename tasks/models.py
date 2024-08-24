4cf01cf55bc91817df0a4e2cf6eb601f7f12f282
earnify123 <earnify123@gmail.com>
2024-08-24 08:23:13 +0600

    Update models.py

--- tasks/models.py
+++ tasks/models.py
@@ -1,1 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    points = models.IntegerField(default=0)
+    completed_by = models.ManyToManyField(User, through='TaskCompletion')
+
+    def __str__(self):
+        return self.title
+
+class TaskCompletion(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    completed_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'task')
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    points = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user.username
 
